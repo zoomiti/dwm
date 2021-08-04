5379cc68a3acafb1d54109583ee62db0e3aa9efa
zoomiti <sotoluis90@gmail.com>
2021-08-03 23:50:17 -0700

    Added pulsemixer shortcut

--- config.h
+++ config.h
@@ -66,6 +66,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_orange, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
+static const char *audiocmd[] = { "urxvt", "-e", "pulsemixer", NULL };
 static const char *browsercmd[] = { "microsoft-edge-dev", NULL };
 static const char *discordcmd[] = { "discord", NULL };
 
@@ -104,6 +105,7 @@ static Key keys[] = {
 	{ 0,                           	XF86XK_AudioRaiseVolume, spawn,          {.v = upvol } },
 	{ 0,                           	XF86XK_AudioLowerVolume, spawn,          {.v = downvol } },
 	{ 0,                           	XF86XK_AudioMute,        spawn,          {.v = mutevol } },
+	{ MODKEY|ShiftMask,             XF86XK_AudioMute,        spawn,          {.v = audiocmd } },
 	TAGKEYS(                        XK_1,                                     0)
 	TAGKEYS(                        XK_2,                                     1)
 	TAGKEYS(                        XK_3,                                     2)
