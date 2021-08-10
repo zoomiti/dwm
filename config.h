6ead6cc5f7656450165b028982c6414dcf879627
zoomiti <sotoluis90@gmail.com>
2021-08-10 00:15:17 -0700

    Added fullgaps

--- config.h
+++ config.h
@@ -4,6 +4,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -102,7 +103,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period,               focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,                tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period,               tagmon,         {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_f,                    fullscreen,     {0} },
+	{ MODKEY,                       XK_minus,                setgaps,        {.i = -5 } },
+	{ MODKEY,                       XK_equal,                setgaps,        {.i = +5 } },
+	{ MODKEY|ShiftMask,             XK_minus,                setgaps,        {.i = GAP_RESET } },
+	{ MODKEY|ShiftMask,             XK_equal,                setgaps,        {.i = GAP_TOGGLE} },
 	{ 0,                           	XF86XK_AudioRaiseVolume, spawn,          {.v = upvol } },
 	{ 0,                           	XF86XK_AudioLowerVolume, spawn,          {.v = downvol } },
 	{ 0,                           	XF86XK_AudioMute,        spawn,          {.v = mutevol } },
