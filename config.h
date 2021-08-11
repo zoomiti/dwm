21aabc0347517c3798788f1a88d1ec7031751ac8
zoomiti <sotoluis90@gmail.com>
2021-08-11 00:20:35 -0700

    Switched back to st

--- config.h
+++ config.h
@@ -66,8 +66,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_orange, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "urxvt", NULL };
-static const char *audiocmd[] = { "urxvt", "-e", "pulsemixer", NULL };
+static const char *termcmd[]  = { "st", NULL };
+static const char *audiocmd[] = { "st", "-e", "pulsemixer", NULL };
 static const char *browsercmd[] = { "microsoft-edge-dev", NULL };
 static const char *discordcmd[] = { "discord", NULL };
 
