6a9279ee576d42279d60c710aa909b738f8c3119
zoomiti <sotoluis90@gmail.com>
2021-09-18 00:28:53 -0400

    Added crunchyroll to entertainment script

--- entertainment.sh
+++ entertainment.sh
@@ -1,5 +1,5 @@
 
-choice=$(dmenu "$@" -l 5 < <(echo $'https://www.youtube.com\nhttps://www.disneyplus.com\nhttps://www.netflix.com\nhttps://www.dropout.tv'))
+choice=$(dmenu "$@" -l 5 < <(echo $'https://www.youtube.com\nhttps://www.disneyplus.com\nhttps://www.netflix.com\nhttps://www.dropout.tv\nhttps://www.crunchyroll.com'))
 
 if [ ! -z "$choice" ] ; 
 then
