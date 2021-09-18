2e1eee1dd13c620ee25f9fe3cd4fc5a5aefd4314
zoomiti <sotoluis90@gmail.com>
2021-09-18 00:19:08 -0400

    Switched to using JetBrains Mono

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12", "fontawesome:size=12" , "fontawesomebrands:size=12" };
+static const char *fonts[]          = { "jetbrains mono:size=12","monospace:size=12", "fontawesome:size=12" , "fontawesomebrands:size=12" };
 static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
